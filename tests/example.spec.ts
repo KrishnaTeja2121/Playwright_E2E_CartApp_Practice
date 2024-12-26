7a595bc19fe73ba845215c1da10946772fb33179
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2024-12-27 00:06:05 +0530

    #2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
+import { TIMEOUT } from 'dns';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -25,8 +27,9 @@ test.only('End 2 End Rahul Shetty Cart Application Practice',async({browser,page
  const loginBtn=page.locator("#login");
  const products=page.locator(".card-body");
  const productName="IPHONE 13 PRO";
+ const email="test21@test.com";
 
- await userName.fill("test21@test.com");
+ await userName.fill(email);
  await userPassword.fill("Test1234");
  await loginBtn.click();
  await page.waitForLoadState('networkidle');
@@ -41,9 +44,35 @@ test.only('End 2 End Rahul Shetty Cart Application Practice',async({browser,page
   }  
  }
  await page.locator("[routerlink='/dashboard/cart']").click();
- await page.locator("h3:has-text(productName)").isVisible();
-
- 
-
-
+ await page.locator("div li").first().waitFor();
+ const bool=await page.locator("h3:has-text('"+productName+"')").isVisible();
+ expect(bool).toBeTruthy();
+ await page.locator("text=Checkout").click();
+ await page.locator(".input").first().fill("4534 3647 6578 1234");
+ await page.locator("[placeholder*='Country']").pressSequentially("ind");
+ const dropDown=page.locator(".ta-results");
+ await dropDown.waitFor();
+ const optionsCount=await dropDown.locator("button").count();
+ for(let i=0;i<count;++i){
+  const text=await dropDown.locator("button").nth(i).textContent();
+  if(text===" India"){
+    dropDown.locator("button").nth(i).click();
+    break;
+  }
+ }
+ await expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+ await page.locator(".action__submit").click();
+ await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+ const orderId= await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+ console.log(orderId);
+ await page.locator("button[routerlink*='myorders']").click();
+ const rows=await page.locator("tbody tr");
+ await page.locator("tbody").waitFor();
+ for(let i=0;i < await rows.count();++i){
+  const order=await rows.nth(i).locator("th").textContent();
+  if(orderId === order){
+    await rows.nth(i).locator("button").first().click();
+    break;
+  }
+ }
 });
